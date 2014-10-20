d679f63bd9dd8951d17f5de319f9f988e2d4bb18
Anthony Ettinger <anthony@chovy.com>
2014-10-19 22:50:09 -0700

    add compress and concat to build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var del = require('del');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 gulp.task('default', [
   'build'
 ]);
 
+//todo add minify
 gulp.task('build', [
   'build:dist'
 ]);
@@ -15,13 +19,25 @@ gulp.task('clean:dist', function(cb){
 
 gulp.task('copy:dist', ['clean:dist'], function(){
   gulp.src([
-    'package.json',
-    '.bowerrc',
+    //'package.json',
     'bower.json'
   ], { base: './' })
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build:dist', ['build', 'copy:dist'], function(){
+gulp.task('concat', function() {
+  gulp.src('src/**/*.js')
+    .pipe(concat('angular-html5-storage.js'))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('compress', function() {
+  gulp.src('src/**/*.js')
+    .pipe(uglify())
+    .pipe(concat('angular-html5-storage.min.js'))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('build:dist', ['copy:dist', 'concat', 'compress'], function(){
   console.log('build:dist finished!');
 });

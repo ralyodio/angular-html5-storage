e1d0294115b11ccc6bcd604595574fd30b0bfa54
Anthony Ettinger <anthony@chovy.com>
2014-11-05 14:33:21 -0800

    add bower.json and dependencies
    - version 0.0.2

--- src/angular-html5-storage.js
+++ src/angular-html5-storage.js
@@ -1,31 +1,35 @@
 'use strict';
 
-angular.module('angular-html5-storage', []);
-angular.module('angular-html5-storage')
-  .factory('ngHTML5Storage', ['$q', function($q){
-    function store(key, value, storeType){
-      var store = storeType === 'session' ? window.sessionStorage : window.localStorage;
-
-      if ( !store ) return;
-
-      if ( key && !value ) {
-        return angular.fromJson(store.getItem(key));
-      }
-
-      if ( key && value ) {
-        store.setItem(key, angular.toJson(value));
-
-        return angular.fromJson(store.getItem(key));
-      }
-    }
-
-    function remove(key, storeType){
+angular.module('ngHTML5Storage', []);
+angular.module('ngHTML5Storage')
+  .factory('$webStoreRemove', ['$q', function($q){
+    return function (key, storeType){
       var store = storeType === 'session' ? window.sessionStorage : window.localStorage;
 
       if ( !store ) return;
 
       return store.removeItem(key);
     }
+  }])
+  .factory('$webStoreSet', ['$q', function($q){
+    var store = storeType === 'session' ? window.sessionStorage : window.localStorage;
+
+    if ( !store ) return;
+
+    if ( key && !value ) {
+      return angular.fromJson(store.getItem(key));
+    }
+
+    if ( key && value ) {
+      store.setItem(key, angular.toJson(value));
+
+      return angular.fromJson(store.getItem(key));
+    }
+  }])
+  .factory('$sessionStorage', [function(){
+
+  }])
+  .factory('$localStorage', ['$q', '$store', function($q){
 
     return {
       local: function(key, value){
